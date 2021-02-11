2d4a7a13a86fd7ec86a7b4281b50f593c34a2877
Senthil kumar Madasamy <mail2senthilkumar@yahoo.com>
2021-02-11 12:17:40 -0800

    added back App.js and package.json

---
+++ pjs-voter-system/src/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { VoterTableContainer } from './containers/VoterTableContainer';
+import { voterToolStore } from './stores/voterToolStore';
+
+import { Layout } from './components/ux/Layout'
+import { ToolHeader } from './components/ux/ToolHeader'
+import { ToolFooter } from './components/ux/ToolFooter'
+import { Sidebar } from './components/ux/Sidebar';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Layout>
+          <ToolHeader headerText="PJS Voter System" />
+          <Provider store={voterToolStore}>
+
+            <aside><Sidebar></Sidebar></aside>
+            <main>
+              <VoterTableContainer/>
+            </main>
+
+          </Provider>
+
+          <ToolFooter footerText="PJS, Inc." />
+        </Layout>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
